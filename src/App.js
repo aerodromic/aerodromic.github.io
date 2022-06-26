712c7b678c8e67c167ab2c571e082d76ffab90d1
Joel Lo <9198061+aerodromic@users.noreply.github.com>
2022-06-26 23:13:30 +0800

    NavBar Creation

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
+import React from "react";
+import Navigation from "./components/Navigation";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import routes from "./routes";
+import { useStyles } from "./styles";
 
-function Copyright() {
+const App = () => {
+  const classes = useStyles();
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
+    <div className={classes.appRoot}>
+      <Router>
+        <Navigation />
+        <div>
+          <div className={classes.appBarSpacer}></div>
+          <Switch>
+            {routes.map((route, index) => {
+              return (
+                <Route exact key={index} path={route.path}>
+                  {route.component}
+                </Route>
+              );
+            })}
+          </Switch>
+        </div>
+      </Router>
+    </div>
   );
-}
+};
 
-export default function App() {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App example
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
-}
+export default App;
